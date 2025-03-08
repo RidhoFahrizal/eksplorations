e4095cb9b9fa41b8294b7500dfb7db99f7159a91
RidhoFahrizal <farizalridho@gmail.com>
2025-03-08 17:33:57 +0700

    Menyelesaikan project book apps

--- book_app/lib/detail_page.dart
+++ book_app/lib/detail_page.dart
@@ -26,8 +26,68 @@ class DetailPage extends StatelessWidget {
               child: Center(child: Image.asset(book.image, width: 130)),
             ),
           ),
+          Center(
+            child: Padding(
+              padding: const EdgeInsets.symmetric(vertical: 20),
+              child: Text(
+                book.name,
+                style: const TextStyle(
+                  fontSize: 30,
+                  fontWeight: FontWeight.w600,
+                ),
+              ),
+            ),
+          ),
+
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+            children: [
+              bookInfo(book.rate.toString(), "Rating"),
+              bookInfo(book.page.toString(), "Page"),
+              bookInfo(book.language, "Language"),
+            ],
+          ),
+
+          const Padding(
+            padding: EdgeInsets.all(8.0),
+            child: Text(
+              "Description",
+              style: TextStyle(fontSize: 20, fontWeight: FontWeight.w600),
+            ),
+          ),
+
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: bookDesc(book.description),
+          ),
         ],
       ),
+    );
+  }
+
+  Widget bookInfo(String value, String info) {
+    return Column(
+      children: [
+        Text(
+          value,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+        Text(
+          info,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w500),
+        ),
+      ],
+    );
+  }
+
+  Widget bookDesc(String desc) {
+    return Column(
+      children: [
+        Text(
+          desc,
+          style: const TextStyle(fontSize: 18, fontWeight: FontWeight.w600),
+        ),
+      ],
     );
   }
 }
